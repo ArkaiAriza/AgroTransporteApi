0fae2609047c142374aa56c6e4f123def87dcf1e
miacastroco <miacastroco@unal.edu.co>
2020-10-04 20:56:46 -0500

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -14,13 +14,16 @@ module.exports = (app) => {
 
   app.get(
     '/auth/google/callback',
-    passport.authenticate('google'),
+    passport.authenticate('google', {
+      successRedirect: '/auth/google/redirect',
+      failureRedirect: '/auth/google',
+    }),
     (req, res, next) => {
       res.send(req.user);
     }
   );
 
   app.get('/auth/google/redirect', async (req, res, next) => {
-    res.redirect('exp://192.168.0.103:19000');
+    res.redirect('exp://');
   });
 };
